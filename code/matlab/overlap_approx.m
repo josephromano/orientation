fb3d51022f6c954067454c58cdf944d4b3fa85f1
josephromano <joseph.d.romano@gmail.com>
2026-06-23 19:11:23 +0200

    fixed rotation of plane due to curvature of earth

--- code/matlab/overlap_approx.m
+++ code/matlab/overlap_approx.m
@@ -10,12 +10,16 @@ close all
 
 c = 299792458;   % speed of light (m/s)
 
-# discrete frequencies
+% discrete frequencies
 N = 4096;
 f = linspace(1, 512, N);
 
-% convert separation distance to m and calculate fstar
+% convert separation distance to m 
 s = s*1000; 
+
+% then calculate Delta and fstar
+RE = 6.37e6; % radius of Earth
+Delta = s/RE; % only approximate
 fstar = c/(2*s);
 
 % convert angles to radians
@@ -25,7 +29,8 @@ Na = length(angles);
 figure(1)
 for ii = 1:1:Na
   % note that sinc(x) = sin(pi x)/(pi x) in matlab
-  orf = cos(2*angles(ii)) * sinc(f/fstar); 
+  % so divide argument by pi to get the sinc(x) = sin(x)/x definition
+  orf = cos(2*angles(ii)) * 0.5 * (1 + (cos(Delta))^2) * sinc(f/fstar); 
   semilogx(f, orf);
   hold on
 end
